23120f7bed504828c186be47518065c6e3c4c10a
Stefan Kummerlöw <gummiz@gmx.net>
2023-04-21 17:40:23 +0200

    Tile Checkbox added

--- assets/script.js
+++ assets/script.js
@@ -1,100 +1,108 @@
 function generatePromptOutput() {
-    const mainIdea = document.getElementById('mainIdea').value;
-    
-    const aspectRatio = document.getElementById('aspectRatio').value;
-    
-    const medium = document.getElementById('medium').value;
-    
-    const camera = document.getElementById('camera').value;
-    
-    const lighting = document.getElementById('lighting').value;
-    
-    const descriptor = document.getElementById('descriptor').value;
-    const descriptor2 = document.getElementById('descriptor2').value;
+  const mainIdea = document.getElementById('mainIdea').value;
 
-    const backgroundInput = document.getElementById('background');
-    const background = backgroundInput.value ? `on ${backgroundInput.value} background` : '';
-    
-    const artistInput = document.getElementById('artist');
-    const artist = artistInput.value ? `by ${artistInput.value}` : '';
-    
-    const filmInput = document.getElementById('film');
-    const film = filmInput.value ? `${filmInput.value} film style` : '';
+  const aspectRatio = document.getElementById('aspectRatio').value;
 
+  const medium = document.getElementById('medium').value;
 
+  const camera = document.getElementById('camera').value;
 
-    const promptOutput = `/imagine prompt: ${medium} of ${mainIdea} ${artist} ${camera} ${background} ${descriptor2} ${descriptor} ${lighting} ${film} --ar ${aspectRatio} --v 5`;
+  const lighting = document.getElementById('lighting').value;
 
-    document.getElementById('promptOutput').value = promptOutput.trim();
+  const descriptor = document.getElementById('descriptor').value;
+  const descriptor2 = document.getElementById('descriptor2').value;
 
-    // After setting the prompt output value, update the copy button visibility
-    toggleCopyButtonVisibility();
-  }
+  const backgroundInput = document.getElementById('background');
+  const background = backgroundInput.value ? `on ${backgroundInput.value} background` : '';
 
-  function init() {
+  const artistInput = document.getElementById('artist');
+  const artist = artistInput.value ? `by ${artistInput.value}` : '';
 
+  const filmInput = document.getElementById('film');
+  const film = filmInput.value ? `${filmInput.value} film style` : '';
 
-    document.getElementById('promptOutput').addEventListener('input', toggleCopyButtonVisibility);
+  // Tile Checkbox 
+  const tileInput = document.getElementById('tile');
+  let tile = '';
 
-    // Call the function initially to set the button visibility on the first load
-    toggleCopyButtonVisibility();
+  if (tileInput.checked) {
 
-    // Hide Output once Input gets new content
-    var inputTextarea = document.getElementById("mainIdea");
-    var outputTextarea = document.getElementById("promptOutput");
-    var myButton = document.getElementById("copyButton");
-
-    inputTextarea.addEventListener("input", function () {
-      outputTextarea.value = "";
-      myButton.textContent = "Copy Prompt";
-      myButton.style.display = "none";
-    });
-
-
-
-    
-  }
-
-  window.onload = init;
-
-  function toggleCopyButtonVisibility() {
-    const outputField = document.getElementById('promptOutput');
-    const copyButton = document.getElementById('copyButton');
-    if (outputField.value.trim() === '') {
-      copyButton.style.display = 'none';
-    } else {
-      copyButton.style.display = 'inline-block';
-    }
+    tile = "--tile";
   }
 
 
-  function copyPromptOutput() {
-    const copyText = document.getElementById('promptOutput');
-    copyText.select();
-    document.execCommand('copy');
+  const promptOutput = `/imagine prompt: ${medium} of ${mainIdea} ${artist} ${camera} ${background} ${descriptor2} ${descriptor} ${lighting} ${film} ${tile} --ar ${aspectRatio} --v 5`;
 
-    // alert('Copied the prompt output: ' + copyText.value);
+  document.getElementById('promptOutput').value = promptOutput.trim();
 
+  // After setting the prompt output value, update the copy button visibility
+  toggleCopyButtonVisibility();
+}
 
-    // // show notification
-    // var promptOutput = document.getElementById("promptOutput");
-    // promptOutput.select();
-    // document.execCommand("copy");
-    // var notification = document.getElementById("copyNotification");
-    // notification.innerHTML = "Prompt copied";
-    // notification.classList.add("show");
-    // setTimeout(function () {
-    //   notification.classList.remove("show");
-    // }, 3000);
+function init() {
 
 
-    // Get the button element
-    var button = document.getElementById("copyButton");
+  document.getElementById('promptOutput').addEventListener('input', toggleCopyButtonVisibility);
 
-    // Change the text to the checkmark symbol
-    button.innerHTML = "&#10003; Prompt Copied";
+  // Call the function initially to set the button visibility on the first load
+  toggleCopyButtonVisibility();
 
-    // Set the font family to a font that supports the checkmark symbol
-    button.style.fontFamily = "Arial, sans-serif";
+  // Hide Output once Input gets new content
+  var inputTextarea = document.getElementById("mainIdea");
+  var outputTextarea = document.getElementById("promptOutput");
+  var myButton = document.getElementById("copyButton");
 
+  inputTextarea.addEventListener("input", function () {
+    outputTextarea.value = "";
+    myButton.textContent = "Copy Prompt";
+    myButton.style.display = "none";
+  });
+
+
+
+
+}
+
+window.onload = init;
+
+function toggleCopyButtonVisibility() {
+  const outputField = document.getElementById('promptOutput');
+  const copyButton = document.getElementById('copyButton');
+  if (outputField.value.trim() === '') {
+    copyButton.style.display = 'none';
+  } else {
+    copyButton.style.display = 'inline-block';
   }
+}
+
+
+function copyPromptOutput() {
+  const copyText = document.getElementById('promptOutput');
+  copyText.select();
+  document.execCommand('copy');
+
+  // alert('Copied the prompt output: ' + copyText.value);
+
+
+  // // show notification
+  // var promptOutput = document.getElementById("promptOutput");
+  // promptOutput.select();
+  // document.execCommand("copy");
+  // var notification = document.getElementById("copyNotification");
+  // notification.innerHTML = "Prompt copied";
+  // notification.classList.add("show");
+  // setTimeout(function () {
+  //   notification.classList.remove("show");
+  // }, 3000);
+
+
+  // Get the button element
+  var button = document.getElementById("copyButton");
+
+  // Change the text to the checkmark symbol
+  button.innerHTML = "&#10003; Prompt Copied";
+
+  // Set the font family to a font that supports the checkmark symbol
+  button.style.fontFamily = "Arial, sans-serif";
+
+}
