56e2fa680ea400fbd39a57e10cb381c18d9ed0d2
Stefan Kummerlöw <gummiz@gmx.net>
2023-06-23 12:50:55 +0200

    remove model number from output

--- assets/script.js
+++ assets/script.js
@@ -57,7 +57,7 @@ function generatePromptOutput() {
   }
 
   // Output
-  const promptOutput = `/imagine prompt: ${medium} ${mainIdea} ${background} ${artist} ${view} ${camera} ${lens} ${films} ${descriptor} ${descriptor2} ${lighting} ${film} ${tile} --ar ${aspectRatio} --v 5.1`;
+  const promptOutput = `/imagine prompt: ${medium} ${mainIdea} ${background} ${artist} ${view} ${camera} ${lens} ${films} ${descriptor} ${descriptor2} ${lighting} ${film} ${tile} --ar ${aspectRatio}`;
 
   document.getElementById('promptOutput').value = promptOutput.trim();
 
